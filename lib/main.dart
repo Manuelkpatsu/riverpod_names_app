7d04cf24b33065d821829ca8f0c56efa033a6da6
Emmanuel Ahuno <manuelkpatsu@gmail.com>
2024-01-27 09:07:32 +0000

    Add stream provider for streaming names

--- lib/main.dart
+++ lib/main.dart
@@ -24,13 +24,53 @@ class MyApp extends StatelessWidget {
   }
 }
 
+const names = [
+  'Alice',
+  'Bob',
+  'Charlie',
+  'David',
+  'Eve',
+  'Fred',
+  'Ginny',
+  'Harriet',
+  'Ileana',
+  'Joseph',
+  'Kincaid',
+  'Larry'
+];
+
+final tickerProvider = StreamProvider(
+  (ref) => Stream.periodic(const Duration(seconds: 1), (i) => i + 1),
+);
+
+final namesProvider = StreamProvider(
+  (ref) => ref.watch(tickerProvider.future).asStream().map((count) => names.getRange(0, count)),
+);
+
 class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final names = ref.watch(namesProvider);
+
     return Scaffold(
       appBar: AppBar(title: const Text('Display Names')),
+      body: names.when(
+        data: (names) {
+          return ListView.builder(
+            itemCount: names.length,
+            itemBuilder: (ctx, index) {
+              return ListTile(title: Text(names.elementAt(index)));
+            },
+          );
+        },
+        error: (error, stackTrace) => const Padding(
+          padding: EdgeInsets.all(16),
+          child: Text('Reached the end of the list'),
+        ),
+        loading: () => const Center(child: CircularProgressIndicator()),
+      ),
     );
   }
 }
